a8490f6f62ad3faf6153d07d7ef31ccaf839bc02
romans1995 <cboby26@gmail.com>
2022-05-30 21:01:25 +0300

    feature:overlay

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@
 
 const nameInput = document.querySelector(".popup__inputs_name");
 const aboutInput = document.querySelector(".popup__inputs_description");
-
+const popoverlay = document.querySelector(".popup__overlay");
 
 const profilInput = document.querySelector(".popup__inputs_submit-button");
 const EditModeName = document.querySelector(".profile__edit");
@@ -13,12 +13,14 @@ const popup = document.querySelector("#popup");
 
 const editProfileFunction = () => {
     openpopup();
+    overlaypopup();
     nameInput.value = profileName.textContent;
     aboutInput.value = profileAbout.textContent;
 }
 const handleProfileEditSubmit = (event) => {
     event.preventDefault();
     closePopUp();
+    popoverlay.style.display = "none";
     profileName.textContent = nameInput.value;
     profileAbout.textContent = aboutInput.value;
 };
@@ -31,10 +33,12 @@ const popupCloseBtn = document.querySelector(".popup__close-btn");
 const openpopup = (event) => {
     popup.classList.toggle("popup__active");
 }
-const overlaypopup = ()=>{
-
+const overlaypopup = () => {
+    popoverlay.style.display = "none" ? popoverlay.style.display = "block" : popoverlay.style.display = "block";
 }
 const closePopUp = () => {
     popup.classList.remove("popup__active");
+    popoverlay.style.display = "none";
+
 }
-popupCloseBtn.addEventListener("click", closePopUp);
+popupCloseBtn.addEventListener("click", closePopUp, overlaypopup);
