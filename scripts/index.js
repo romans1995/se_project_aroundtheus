0467b75b1bd102ff6a0ab8afcec71a637c3da085
romans1995 <cboby26@gmail.com>
2022-06-01 10:58:43 +0300

    fix:form

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,10 @@
 // popup
-
 const nameInput = document.querySelector(".popup__input_type_name");
 const aboutInput = document.querySelector(".popup__input_type_description");
-const popoverlay = document.querySelector(".popup");
-
+const popOverlay = document.querySelector(".popup");
 const profilInputSubmit = document.querySelector(".popup__inputs_submit-button");
-const EditModeName = document.querySelector(".profile__edit");
-
+const form = document.querySelector(".popup__inputs-container");
+const editModeName = document.querySelector(".profile__edit");
 const profileName = document.querySelector(".profile__description-name");
 const profileAbout = document.querySelector(".profile__description-prof");
 const popup_container = document.querySelector(".popup__container");
@@ -17,25 +15,24 @@ const editProfileFunction = () => {
     nameInput.value = profileName.textContent;
     aboutInput.value = profileAbout.textContent;
 }
-const handleProfileEditSubmit = (event) => {
-    event.preventDefault();
-
-    popoverlay.style.display = "none";
+const handleProfileEditSubmit = event => {
+    popOverlay.style.display = "none";
     profileName.textContent = nameInput.value;
     profileAbout.textContent = aboutInput.value;
     closePopUp();
+    event.preventDefault();
 };
 
 const openPopup = () => {
     popup_container.classList.toggle("popup_active");
-    popoverlay.style.display = "block"
+    popOverlay.style.display = "block"
 }
 
 const closePopUp = () => {
     popup_container.classList.remove("popup_active");
-    popoverlay.style.display = "none";
-
+    popOverlay.style.display = "none";
 }
-EditModeName.addEventListener("click", editProfileFunction);
-profilInputSubmit.addEventListener("submit", handleProfileEditSubmit);
+
+editModeName.addEventListener("click", editProfileFunction);
+form.addEventListener("submit", handleProfileEditSubmit);
 popupCloseBtn.addEventListener("click", closePopUp);
