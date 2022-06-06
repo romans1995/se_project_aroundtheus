db76b5592ad8232299a0d2142e60b96da365b3c7
romans1995 <44429106+romans1995@users.noreply.github.com>
2022-06-06 14:13:50 +0300

    feature:elments +popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,29 @@
+const initialelements = [
+    {
+      name: "Yosemite Valley",
+      link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+    },
+    {
+      name: "Lake Louise",
+      link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+    },
+    {
+      name: "Bald Mountains",
+      link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+    },
+    {
+      name: "Latemar",
+      link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+    },
+    {
+      name: "Vanoise National Park",
+      link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+    },
+    {
+      name: "Lago di Braies",
+      link: "https://code.s3.yandex.net/web-code/lago.jpg"
+    }
+  ]; 
 // popup
 
 const nameInput = document.querySelector(".popup__inputs_type_name");
@@ -8,11 +34,18 @@ const editModeName = document.querySelector(".profile__edit");
 const profileName = document.querySelector(".profile__description-name");
 const profileAbout = document.querySelector(".profile__description-prof");
 const popupCloseBtn = document.querySelector(".popup__close-btn");
+//end popup
+
+//elements
+const elements = document.querySelector(".elements");
+const elementList = elements.querySelector(".elements__list");
+//image preview
+const imgPrevModal = document.querySelector(".popup_image-prev");
 
 //popup functions
 
 const editProfileFunction = () => {
-    openPopup();
+    openPopup(popOverlay);
     nameInput.value = profileName.textContent;
     aboutInput.value = profileAbout.textContent;
 }
@@ -25,8 +58,8 @@ const handleProfileEditSubmit = event => {
 
 };
 
-const openPopup = () => {
-    popOverlay.classList.add("popup_active");
+const openPopup = (elem) => {
+    elem.classList.add("popup_active");
 
 }
 
@@ -34,6 +67,42 @@ const closePopUp = () => {
     popOverlay.classList.remove("popup_active");
 
 }
+
+//elements functions 
+const addelement = (data) => {
+    const elementStracture = document.querySelector(".element-Stracture").content;
+    const elementElement = elementStracture.querySelector(".element").cloneNode(true);
+    const elementTitleElement = elementElement.querySelector(".element__title");
+    elementTitleElement.textContent = data.name;
+    const likeBtn = elementElement.querySelector(".element__like-button");
+    const elementImage = elementElement.querySelector(".element__image");
+    elementImage.src = data.link;
+    elementImage.alt = `picture of a ${data.name}`;
+    const deleteBtn = elementElement.querySelector(".element__delete-button");
+    //eventListeners
+    elementImage.addEventListener("click", () => previewImage(data));
+    deleteBtn.addEventListener("click", () => elementElement.remove());
+    likeBtn.addEventListener("click", () =>
+    addClass(likeBtn, "element__like-button_active")
+    );
+    return elementElement;
+};
+const addAllelements = (element, list) => {
+    list.prepend(addelement(element));
+};
+
+initialelements.forEach((element) => addAllelements(element, elementList));
+
+const previewImage = (element) => {
+    const popupImage = imgPrevModal.querySelector(".popup__image");
+    const popupCaption = imgPrevModal.querySelector(".popup__caption");
+    popupImage.src = element.link;
+    popupImage.alt = `A beautiful place in ${element.name}`;
+    popupCaption.textContent = element.name;
+    openPopup(imgPrevModal);
+};
+
+
 
 //popup eventsListeners
 
