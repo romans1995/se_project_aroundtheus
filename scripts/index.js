438f66cae3bb2dfb8ff2324621aa0f334acb289e
romans1995 <cboby26@gmail.com>
2022-06-13 23:07:28 +0300

    fix:js

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,27 @@
-const initialelements = [
-  {
-    name: "Yosemite Valley",
-    link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
-  },
-  {
-    name: "Lake Louise",
-    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
-  },
-  {
-    name: "Bald Mountains",
-    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
-  },
-  {
-    name: "Latemar",
-    link: "https://code.s3.yandex.net/web-code/latemar.jpg"
-  },
-  {
-    name: "Vanoise National Park",
-    link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
-  },
-  {
-    name: "Lago di Braies",
-    link: "https://code.s3.yandex.net/web-code/lago.jpg"
-  }
+const initialelements = [{
+        name: "Yosemite Valley",
+        link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+    },
+    {
+        name: "Lake Louise",
+        link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+    },
+    {
+        name: "Bald Mountains",
+        link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+    },
+    {
+        name: "Latemar",
+        link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+    },
+    {
+        name: "Vanoise National Park",
+        link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+    },
+    {
+        name: "Lago di Braies",
+        link: "https://code.s3.yandex.net/web-code/lago.jpg"
+    }
 ];
 
 // popup
@@ -30,9 +29,9 @@ const initialelements = [
 const nameInput = document.querySelector(".popup__inputs_type_name");
 const aboutInput = document.querySelector(".popup__inputs_type_description");
 const profilePopup = document.querySelector(".profile-popup");
-const popOverlayadd = document.querySelector(".popup-addElement");
+const cardPopupAdd = document.querySelector(".popup-addElement");
 const editPopupForm = document.querySelector(".popup__inputs-container");
-const editPopupFormaddPlace = document.querySelector(".popup__inputs-container-addPlace");
+const editPopupFormAddPlace = document.querySelector(".popup__inputs-container-addPlace");
 const editModeName = document.querySelector(".profile__edit");
 const profileName = document.querySelector(".profile__description-name");
 const profileAbout = document.querySelector(".profile__description-prof");
@@ -55,85 +54,85 @@ const popupCaption = imgPrevModal.querySelector(".popup__caption");
 //popup functions
 
 const editProfile = () => {
-  openPopup(profilePopup);
-  nameInput.value = profileName.textContent;
-  aboutInput.value = profileAbout.textContent;
+    openPopup(profilePopup);
+    nameInput.value = profileName.textContent;
+    aboutInput.value = profileAbout.textContent;
 }
 
 const handleProfileEditSubmit = event => {
-  event.preventDefault();
-  profileName.textContent = nameInput.value;
-  profileAbout.textContent = aboutInput.value;
-  closePopUp(popOverlay);
+    event.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileAbout.textContent = aboutInput.value;
+    closePopUp(profilePopup);
 
 };
-const addPlaceFunction = () => {
-  openPopup(popOverlayadd);
+const openCardPopup = () => {
+    openPopup(cardPopupAdd);
 }
 
-const handLeadPlaceSubmit = event => {
-  event.preventDefault();
-  const newElement = {
-    name: `${placeName.value}`,
-    link: `${placeLink.value}`
-  };
-  closePopUp(popOverlayadd);
-  prependCard(newElement, elementList);
-  placeName.value = "";
-  placeLink.value = "";
+const handleAddPlaceSubmit = event => {
+    event.preventDefault();
+    const newElement = {
+        name: `${placeName.value}`,
+        link: `${placeLink.value}`
+    };
+    closePopUp(cardPopupAdd);
+    prependCard(newElement, elementList);
+    placeName.value = "";
+    placeLink.value = "";
 };
 
 const openPopup = (elem) => {
-  elem.classList.add("popup_active");
+    elem.classList.add("popup_active");
 
 }
 
 const closePopUp = (elem) => {
-  elem.classList.remove("popup_active");
+    elem.classList.remove("popup_active");
 }
 
 //elements functions 
 const addElement = (data) => {
-  const elementStracture = document.querySelector(".element-Stracture").content;
-  const elementElement = elementStracture.querySelector(".element").cloneNode(true);
-  const elementTitleElement = elementElement.querySelector(".element__title");
-  elementTitleElement.textContent = data.name;
-  const likeBtn = elementElement.querySelector(".element__like-button");
-  const elementImage = elementElement.querySelector(".element__image");
-  elementImage.src = data.link;
-  elementImage.alt = `picture of a ${data.name}`;
-  const deleteBtn = elementElement.querySelector(".element__delete-button");
-  //eventListeners
-  elementImage.addEventListener("click", () => previewImage(data));
-  deleteBtn.addEventListener("click", () => elementElement.remove());
-  likeBtn.addEventListener("click", () =>
-    toggleClass(likeBtn, "element__like-button_active")
-  );
-  return elementElement;
+    const elementStracture = document.querySelector(".element-Stracture").content;
+    const elementElement = elementStracture.querySelector(".element").cloneNode(true);
+    const elementTitleElement = elementElement.querySelector(".element__title");
+    elementTitleElement.textContent = data.name;
+    const likeBtn = elementElement.querySelector(".element__like-button");
+    const elementImage = elementElement.querySelector(".element__image");
+    elementImage.src = data.link;
+    elementImage.alt = `picture of a ${data.name}`;
+    const deleteBtn = elementElement.querySelector(".element__delete-button");
+    //eventListeners
+    elementImage.addEventListener("click", () => previewImage(data));
+    deleteBtn.addEventListener("click", () => elementElement.remove());
+    likeBtn.addEventListener("click", () =>
+        toggleClass(likeBtn, "element__like-button_active")
+    );
+    return elementElement;
 };
 const prependCard = (element, list) => {
-  list.prepend(addElement(element));
+    list.prepend(addElement(element));
 };
 const toggleClass = (component, add) => {
-  component.classList.toggle(add);
+    component.classList.toggle(add);
 };
 
 const previewImage = (element) => {
-  popupImage.src = element.link;
-  popupImage.alt = `A beautiful place in ${element.name}`;
-  popupCaption.textContent = element.name;
-  openPopup(imgPrevModal);
+    popupImage.src = element.link;
+    popupImage.alt = `A beautiful place in ${element.name}`;
+    popupCaption.textContent = element.name;
+    openPopup(imgPrevModal);
 };
 
 //popup eventsListeners
 
 editModeName.addEventListener("click", editProfile);
-addPlaceBtn.addEventListener("click", addPlaceFunction);
+addPlaceBtn.addEventListener("click", openCardPopup);
 editPopupForm.addEventListener("submit", handleProfileEditSubmit);
-editPopupFormaddPlace.addEventListener("submit", handLeadPlaceSubmit);
+editPopupFormAddPlace.addEventListener("submit", handleAddPlaceSubmit);
 allPopupCloseBtns.forEach((button) => {
-  const popup = button.closest('.popup');
-  button.addEventListener('click', () => closePopUp(popup));
+    const popup = button.closest('.popup');
+    button.addEventListener('click', () => closePopUp(popup));
 });
 
 //element
